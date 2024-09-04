98d0ef0b7d4798f9b269763993bf4dafa3a904f9
MdAAmin <mdabdulamin11@gmail.com>
2024-09-04 21:58:08 +0530

    project

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.task1;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,14 +30,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button = findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
+        resultTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SL2.class);
-                startActivity(intent);
+                resultTextView.setText("");
             }
         });
+
     }
 
     private void calculateBMI() {
